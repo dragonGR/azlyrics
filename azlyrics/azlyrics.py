44920fde39466b4ca1ce2bd9fb8c9e78d178c84c
Marcos Ferreira <merkkp@gmail.com>
2017-03-08 19:19:12 -0300

    same shit, just with a more OO approach

--- azlyrics/azlyrics.py
+++ azlyrics/azlyrics.py
@@ -4,64 +4,65 @@ from bs4 import BeautifulSoup
 
 from azlyrics.cache import Cache
 
-def normalize_artist_music(artist, music):
-    if artist and music:
-        artist = re.sub(r'\W+', '', artist.lower())
-        music = re.sub(r'\W+', '', music.lower())
-    else:
-        artist = "rickastley"
-        music = "nevergonnagiveyouup"
-    return artist, music
+class Azlyrics(object):
 
-def url_from_artist_music(artist, music):
-    artist, music = normalize_artist_music(artist, music)
-    url = "http://azlyrics.com/lyrics/{}/{}.html".format(artist, music)
-    return url
+    def __init__(self, artist, music):
+        self.artist = artist
+        self.music = music
 
-def get_page_from_url(url):
-    try:
-        page = urllib.request.urlopen(url)
-        return page.read() 
-    except urllib.error.HTTPError as e:
-        if e.code == 404:
-            print("Music not found")
-            sys.exit(1)
+    def normalize_str(self, str):
+        return re.sub(r'\W+', '', str.lower())
 
-def get_lyrics_from_page(page):
-    soup = BeautifulSoup(page, "html.parser")
-    lyrics_tags = soup.find_all("div", attrs={"class": None, "id": None})
-    lyrics = [tag.getText() for tag in lyrics_tags]
-    return lyrics
+    def normalize_artist_music(self):
+        return self.normalize_str(self.artist), self.normalize_str(self.music)
 
-def format_lyrics(lyrics):    
-    formated_lyrics = "\n".join(lyrics)
-    return formated_lyrics
 
-def save_lyrics_to_file(path, lyrics):
-    with open(path, "w") as f:
-        f.write(lyrcs)
+    def url(self):
+        if not self.artist and not self.music:
+            self.artist = "rickastley"
+            self.music = "nevergonnagiveyouup"
+        return "http://azlyrics.com/lyrics/{}/{}.html".format(*self.normalize_artist_music())
 
-def get_lyrics(artist, music):
-    url = url_from_artist_music(artist, music)
-    page = get_page_from_url(url)
-    lyrics = get_lyrics_from_page(page)
-    return lyrics
+    def get_page(self):
+        try:
+            page = urllib.request.urlopen(self.url())
+            return page.read()
+        except urllib.error.HTTPError as e:
+            if e.code == 404:
+                print("Music not found")
+                sys.exit(1)
 
-def get_args():
+    def extract_lyrics(self, page):
+        soup = BeautifulSoup(page, "html.parser")
+        lyrics_tags = soup.find_all("div", attrs={"class": None, "id": None})
+        lyrics = [tag.getText() for tag in lyrics_tags]
+        return lyrics
+
+    def format_lyrics(self, lyrics):
+        formated_lyrics = "\n".join(lyrics)
+        return formated_lyrics
+
+    def get_lyrics(self):
+        page = self.get_page()
+        lyrics = self.extract_lyrics(page)
+        return lyrics
+
+
+
+def run():
     parser = argparse.ArgumentParser(description="Fetch lyric from azlyric")
     parser.add_argument("artist", metavar="Artist", type=str)
     parser.add_argument("music", metavar="Music", type=str)
     parser.add_argument("-s", "--save", metavar="path", help="Save to the file", default=False, dest="path")
     args = parser.parse_args()
-    return args
 
-def run():
-    args = get_args()
     c = Cache()
-    cache_key = '_'.join(normalize_artist_music(args.artist, args.music))
+    az = Azlyrics(args.artist, args.music)
+    cache_key = '_'.join(az.normalize_artist_music())
+
     lyrics = c.get(cache_key)
     if lyrics is None:
-        lyrics = format_lyrics(get_lyrics(args.artist, args.music))
+        lyrics = az.format_lyrics(az.get_lyrics())
         c.add(cache_key, lyrics)
     if args.path:
         save_lyrics_to_file(args.path, lyrics)
--- tests/test_azlyrics.py
+++ tests/test_azlyrics.py
@@ -1,29 +1,35 @@
 import unittest, sys
 
-import azlyrics 
+from azlyrics import Azlyrics, Cache
 
 class UrlTest(unittest.TestCase):
 
     def test_normalize_artist_music(self):
-        artist, music = azlyrics.normalize_artist_music("ARTISTA", "muSICA")
+        az = Azlyrics("Artista", "MUsica")
+        artist, music = az.normalize_artist_music()
         self.assertEqual("artista", artist)
         self.assertEqual("musica", music)
 
     def test_url_default_generate_rick_roll(self):
+
         expected = "http://azlyrics.com/lyrics/rickastley/nevergonnagiveyouup.html"
-        self.assertEqual(expected, azlyrics.url_from_artist_music("", ""))
+        az = Azlyrics("", "")
+        self.assertEqual(expected, az.url())
 
     def test_url_uppercase(self):
         expected = "http://azlyrics.com/lyrics/molejo/cilada.html"
-        self.assertEqual(expected, azlyrics.url_from_artist_music("Molejo", "CiLada"))
+        az = Azlyrics("MOLEJO", "CILADA")
+        self.assertEqual(expected, az.url())
 
     def test_url_spaces(self):
         expected = "http://azlyrics.com/lyrics/brunoetrio/garimpo.html"
-        self.assertEqual(expected, azlyrics.url_from_artist_music("Bruno e Trio", "Garimpo"))
+        az = Azlyrics("Bruno e Trio", "Garimpo")
+        self.assertEqual(expected, az.url())
     
-    @unittest.skip
     def test_url_with_special_chars(self):
-        url = ""
+        expected = "http://azlyrics.com/lyrics/legiaourbana/tempoperdido.html"
+        az = Azlyrics("Legião Urbana", "Tempo Perdido")
+        self.assertEqual(expected, az.url())
 
 
 class PageTets(unittest.TestCase):
@@ -38,23 +44,27 @@ class LyricTest(unittest.TestCase):
     def test_lyric_in_one_div(self):
         expected = ["Run, Run"]
         mock_page = "<div>Run, Run</div>"
-        self.assertEqual(expected, azlyrics.get_lyrics_from_page(mock_page));
+        az = Azlyrics("", "")
+        self.assertEqual(expected, az.extract_lyrics(mock_page))
 
     def test_lyric_in_two_divs_alone(self):
         expected = ["Run, Run", "Far Away"]
         mock_page = "<div>Run, Run</div><div>Far Away</div>"
-        self.assertEqual(expected, azlyrics.get_lyrics_from_page(mock_page));
+        az = Azlyrics("", "")
+        self.assertEqual(expected, az.extract_lyrics(mock_page))
 
     def test_lyric_in_two_divs_not_alone(self):
         expected = ["Run, Run", "Far Away"]
         mock_page = "<div>Run, Run</div><p>OKOKOK</p><div>Far Away</div>"
-        self.assertEqual(expected, azlyrics.get_lyrics_from_page(mock_page));
+        az = Azlyrics("", "")
+        self.assertEqual(expected, az.extract_lyrics(mock_page))
 
 class PrintAndSaveTest(unittest.TestCase):
 
     def test_format_lyric(self):
         expected = "blablabla\n2bla2bla2bla"
-        self.assertEqual(expected, azlyrics.format_lyrics(["blablabla", "2bla2bla2bla"]))
+        az = Azlyrics("", "")
+        self.assertEqual(expected, az.format_lyrics(["blablabla", "2bla2bla2bla"]))
 
 class CacheTest(unittest.TestCase):
     
